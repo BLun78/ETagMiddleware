831ab4403c0347d218b96669fa6a857206a2508b
Björn Lundström <viper78@gmx.net>
2018-01-29 07:04:00 +0100

    * ETagMiddleware.csproj: Add first Test
    * UnitTest1.cs:
    * MSTest.csproj:
    * nuget.config:
    * ETagMiddlewareExtensionsTests.cs:

---
+++ MSTest/ETagMiddlewareExtensionsTests.cs
@@ -0,0 +1,83 @@
+using System;
+using BLun.ETagMiddleware;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+
+namespace MSTest
+{
+    [TestClass]
+    public class ETagMiddlewareExtensionsTests
+    {
+        [TestMethod]
+        public void UseETag_Without_Param_Ok()
+        {
+            // arange
+            var app = Substitute.For<IApplicationBuilder>();
+
+            // act
+            app.UseETag();
+
+            // Assert
+            app.Received().Use(Arg.Any<Func<RequestDelegate, RequestDelegate>>());
+            Assert.IsNotNull(app);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException), "app")]
+        public void UseETag_With_NullApp_Without_Param_Ok()
+        {
+            // arange
+            IApplicationBuilder app = null;
+
+            // act
+            app.UseETag();
+
+            // Assert
+            Assert.Fail("No Exception");
+        }
+
+        [TestMethod]
+        public void UseETag_With_Param_Ok()
+        {
+            // arange
+            var app = Substitute.For<IApplicationBuilder>();
+
+            // act
+            app.UseETag(new ETagOption() { BodyMaxLength = ETagMiddlewareExtensions.DefaultBodyMaxLength });
+
+            // Assert
+            app.Received().Use(Arg.Any<Func<RequestDelegate, RequestDelegate>>());
+            Assert.IsNotNull(app);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException), "option")]
+        public void UseETag_With_Null_Param_Ok()
+        {
+            // arange
+            var app = Substitute.For<IApplicationBuilder>();
+
+            // act
+            app.UseETag(null);
+
+            // Assert
+            Assert.Fail("No Exception");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException), "app")]
+        public void UseETag_With_NullApp_With_Param_Ok()
+        {
+            // arange
+            IApplicationBuilder app = null;
+
+            // act
+            app.UseETag(new ETagOption() { BodyMaxLength = ETagMiddlewareExtensions.DefaultBodyMaxLength });
+
+            // Assert
+            Assert.Fail("No Exception");
+        }
+    }
+}
