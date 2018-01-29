6be51befff059ac271572aee98281a61489e19d4
Björn Lundström <viper78@gmx.net>
2018-01-29 21:50:22 +0100

    add test for IsEtagSupported

--- MSTest/ETagMiddlewareTests.cs
+++ MSTest/ETagMiddlewareTests.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.IO;
 using BLun.ETagMiddleware;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.Net.Http.Headers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NSubstitute;
 
@@ -11,7 +13,6 @@ namespace ETagMiddlewareTest
     [TestClass]
     public class ETagMiddlewareTests
     {
-
         internal class TestETagMiddleware : ETagMiddleware
         {
 
@@ -24,6 +25,7 @@ namespace ETagMiddlewareTest
 
             public long BodyMaxLength => this._bodyMaxLength;
             public ILogger<ETagMiddleware> Logger => this._logger;
+            public bool IsEtagSupported(HttpContext context) => base.IsEtagSupported(context);
 
         }
 
@@ -38,6 +40,7 @@ namespace ETagMiddlewareTest
         }
 
         #region Ctor Tests
+
         [TestMethod]
         [ExpectedException(typeof(ArgumentNullException), "next")]
         public void Create_And_Check_Ctor_Exceptions_Next_NOk()
@@ -47,7 +50,7 @@ namespace ETagMiddlewareTest
             // act
             var etag = new ETagMiddleware(null, null, null);
 
-            // Assert
+            // assert
             Assert.Fail("No Exception");
         }
 
@@ -60,7 +63,7 @@ namespace ETagMiddlewareTest
             // act
             var etag = new ETagMiddleware(Substitute.For<RequestDelegate>(), null, null);
 
-            // Assert
+            // assert
             Assert.Fail("No Exception");
         }
 
@@ -75,7 +78,7 @@ namespace ETagMiddlewareTest
             // act
             var etag = new TestETagMiddleware(Substitute.For<RequestDelegate>(), options, Substitute.For<ILoggerFactory>());
 
-            // Assert
+            // assert
             Assert.AreNotEqual(length, etag.BodyMaxLength);
             Assert.AreEqual(ETagMiddlewareExtensions.DefaultBodyMaxLength, etag.BodyMaxLength);
         }
@@ -91,7 +94,7 @@ namespace ETagMiddlewareTest
             // act
             var etag = new TestETagMiddleware(Substitute.For<RequestDelegate>(), options, Substitute.For<ILoggerFactory>());
 
-            // Assert
+            // assert
             Assert.AreEqual(length, etag.BodyMaxLength);
             Assert.AreNotEqual(ETagMiddlewareExtensions.DefaultBodyMaxLength, etag.BodyMaxLength);
         }
@@ -110,7 +113,7 @@ namespace ETagMiddlewareTest
                                           options,
                                           null);
 
-            // Assert
+            // assert
             Assert.Fail("No Exception");
         }
 
@@ -128,10 +131,160 @@ namespace ETagMiddlewareTest
                                           options,
                                           CreateILoggerFactory());
 
-            // Assert
+            // assert
             Assert.IsNotNull(etag.Logger);
             Assert.AreEqual(length, etag.BodyMaxLength);
         }
+
+        #endregion
+
+        #region IsEtagSupported Tests
+
+        [TestMethod]
+        public void IsEtagSupported_Ok()
+        {
+            // arange
+            long length = 100;
+            ETagOption etagOption = new ETagOption() { BodyMaxLength = length };
+            IOptions<ETagOption> options = Options.Create(etagOption);
+            var etag = new TestETagMiddleware(Substitute.For<RequestDelegate>(),
+                                          options,
+                                          CreateILoggerFactory());
+
+            var response = Substitute.For<HttpResponse>();
+            response.Body.Returns(Substitute.For<Stream>());
+            response.Body.Length.ReturnsForAnyArgs(length);
+            response.StatusCode.Returns<int>(200);
+            response.Headers.ContainsKey(HeaderNames.ETag).ReturnsForAnyArgs(false);
+
+            var context = Substitute.For<HttpContext>();
+            context.Response.Returns(response);
+
+            // act
+            var result = etag.IsEtagSupported(context);
+
+            // assert
+            Assert.IsNotNull(etag.Logger);
+            Assert.AreEqual(length, etag.BodyMaxLength);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void IsEtagSupported_StatusCode_100_NOk()
+        {
+            // arange
+            long length = 100;
+            ETagOption etagOption = new ETagOption() { BodyMaxLength = length };
+            IOptions<ETagOption> options = Options.Create(etagOption);
+            var etag = new TestETagMiddleware(Substitute.For<RequestDelegate>(),
+                                          options,
+                                          CreateILoggerFactory());
+
+            var response = Substitute.For<HttpResponse>();
+            response.Body.Returns(Substitute.For<Stream>());
+            response.Body.Length.ReturnsForAnyArgs(length);
+            response.StatusCode.Returns<int>(100);
+            response.Headers.ContainsKey(HeaderNames.ETag).ReturnsForAnyArgs(false);
+
+            var context = Substitute.For<HttpContext>();
+            context.Response.Returns(response);
+
+            // act
+            var result = etag.IsEtagSupported(context);
+
+            // assert
+            Assert.IsNotNull(etag.Logger);
+            Assert.AreEqual(length, etag.BodyMaxLength);
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void IsEtagSupported_StatusCode_300_NOk()
+        {
+            // arange
+            long length = 100;
+            ETagOption etagOption = new ETagOption() { BodyMaxLength = length };
+            IOptions<ETagOption> options = Options.Create(etagOption);
+            var etag = new TestETagMiddleware(Substitute.For<RequestDelegate>(),
+                                          options,
+                                          CreateILoggerFactory());
+
+            var response = Substitute.For<HttpResponse>();
+            response.Body.Returns(Substitute.For<Stream>());
+            response.Body.Length.ReturnsForAnyArgs(length);
+            response.StatusCode.Returns<int>(300);
+            response.Headers.ContainsKey(HeaderNames.ETag).ReturnsForAnyArgs(false);
+
+            var context = Substitute.For<HttpContext>();
+            context.Response.Returns(response);
+
+            // act
+            var result = etag.IsEtagSupported(context);
+
+            // assert
+            Assert.IsNotNull(etag.Logger);
+            Assert.AreEqual(length, etag.BodyMaxLength);
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void IsEtagSupported_BodyMaxLength_NOk()
+        {
+            // arange
+            long length = 100;
+            ETagOption etagOption = new ETagOption() { BodyMaxLength = length };
+            IOptions<ETagOption> options = Options.Create(etagOption);
+            var etag = new TestETagMiddleware(Substitute.For<RequestDelegate>(),
+                                          options,
+                                          CreateILoggerFactory());
+
+            var response = Substitute.For<HttpResponse>();
+            response.Body.Returns(Substitute.For<Stream>());
+            response.Body.Length.ReturnsForAnyArgs(length + 10);
+            response.StatusCode.Returns<int>(200);
+            response.Headers.ContainsKey(HeaderNames.ETag).ReturnsForAnyArgs(false);
+
+            var context = Substitute.For<HttpContext>();
+            context.Response.Returns(response);
+
+            // act
+            var result = etag.IsEtagSupported(context);
+
+            // assert
+            Assert.IsNotNull(etag.Logger);
+            Assert.AreEqual(length, etag.BodyMaxLength);
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void IsEtagSupported_Headers_NOk()
+        {
+            // arange
+            long length = 100;
+            ETagOption etagOption = new ETagOption() { BodyMaxLength = length };
+            IOptions<ETagOption> options = Options.Create(etagOption);
+            var etag = new TestETagMiddleware(Substitute.For<RequestDelegate>(),
+                                          options,
+                                          CreateILoggerFactory());
+
+            var response = Substitute.For<HttpResponse>();
+            response.Body.Returns(Substitute.For<Stream>());
+            response.Body.Length.ReturnsForAnyArgs(length);
+            response.StatusCode.Returns<int>(200);
+            response.Headers.ContainsKey(HeaderNames.ETag).ReturnsForAnyArgs(true);
+
+            var context = Substitute.For<HttpContext>();
+            context.Response.Returns(response);
+
+            // act
+            var result = etag.IsEtagSupported(context);
+
+            // assert
+            Assert.IsNotNull(etag.Logger);
+            Assert.AreEqual(length, etag.BodyMaxLength);
+            Assert.IsFalse(result);
+        }
+
         #endregion
 
     }
